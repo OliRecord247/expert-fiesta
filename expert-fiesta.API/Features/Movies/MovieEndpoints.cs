9d1987298d804ed6372efe867d92507eb3c4759b
Oliver <oliver.verdesseldonck@gmail.com>
2026-02-02 14:34:51 +0100

    Adding API project

---
+++ expert-fiesta.API/Features/Movies/MovieEndpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace expert_fiesta.API.Features.Movies;
+
+public static class MovieEndpoints
+{
+    public static void RegisterMovieEndpoints(this WebApplication app)
+    {
+        var _movieGroup = app.MapGroup("/movies");
+
+        _movieGroup.MapGet("/", () =>
+        {
+            
+        });
+    }
+}
