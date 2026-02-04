0d99b5a7e6a23d2c8b7f1bbea96d3435261bc2dd
Oliver <oliver.verdesseldonck@gmail.com>
2026-02-04 15:01:21 +0100

    Using extended methods for DB, Services and API

--- expert-fiesta.API/Program.cs
+++ expert-fiesta.API/Program.cs
@@ -1,8 +1,16 @@
+using expert_fiesta.API.Features.Games;
+using expert_fiesta.Application;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]!);
 
 var app = builder.Build();
 
@@ -10,7 +18,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
+
+app.UseGameEndpoints();
 
 app.UseHttpsRedirection();
 
