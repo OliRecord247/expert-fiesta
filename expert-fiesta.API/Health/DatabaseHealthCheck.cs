ef9648ce8d7ea5679a7d9ef3eeb76f79125f2ce5
Oliver <oliver.verdesseldonck@gmail.com>
2026-02-18 19:54:44 +0100

    Map health check with database

---
+++ expert-fiesta.API/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using expert_fiesta.Application.Data;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace expert_fiesta.API.Health;
+
+public class DatabaseHealthCheck : IHealthCheck
+{
+    public const string Name = "Database";
+    
+    private readonly ApplicationDbContext _context;
+    private readonly ILogger<DatabaseHealthCheck> _logger;
+
+    public DatabaseHealthCheck(ApplicationDbContext context, ILogger<DatabaseHealthCheck> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, 
+        CancellationToken cancellationToken = new ())
+    {
+        var isValid = await _context.Database.CanConnectAsync(cancellationToken);
+        if (!isValid)
+        {
+            const string errorMessage = "Database is unhealthy";
+            _logger.LogError(errorMessage);
+            return HealthCheckResult.Unhealthy(errorMessage);
+        }
+            
+        return HealthCheckResult.Healthy();
+    }
+}
--- expert-fiesta.API/Program.cs
+++ expert-fiesta.API/Program.cs
@@ -1,4 +1,5 @@
 using expert_fiesta.API.Features.Games;
+using expert_fiesta.API.Health;
 using expert_fiesta.API.Mapping;
 using expert_fiesta.Application;
 using Scalar.AspNetCore;
@@ -10,12 +11,16 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddApplication();
 builder.Services.AddDatabase(config["Database:ConnectionString"]!);
+builder.Services.AddHealthChecks()
+    .AddCheck<DatabaseHealthCheck>(DatabaseHealthCheck.Name);
+
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? [];
+        var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
         policy.WithOrigins(allowedOrigins!)
             .AllowAnyMethod()
             .AllowAnyHeader();
@@ -30,11 +35,11 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
-app.UseCors();
-
-app.UseMiddleware<ValidationMappingMiddleware>();
-app.UseGameEndpoints();
+app.MapHealthChecks("_health");
 
 app.UseHttpsRedirection();
+app.UseCors();
+app.UseMiddleware<ValidationMappingMiddleware>();
+app.UseGameEndpoints();
 
 app.Run();
