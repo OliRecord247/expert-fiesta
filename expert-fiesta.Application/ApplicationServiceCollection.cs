e4fbfcaca39cd055f2deafeca27e4d2e83695a6c
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-05-28 00:32:35 +0200

    New Customer Entity

--- expert-fiesta.Application/ApplicationServiceCollection.cs
+++ expert-fiesta.Application/ApplicationServiceCollection.cs
@@ -9,6 +9,10 @@ public static class ApplicationServiceCollection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IGameService, GameService>();
+        services.AddMediatR(cfg => 
+        {
+            cfg.RegisterServicesFromAssembly(typeof(IApplicationMarker).Assembly);
+        });
         services.AddValidatorsFromAssemblyContaining<IApplicationMarker>();
         return services;
     }
